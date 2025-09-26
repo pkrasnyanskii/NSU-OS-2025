427f04007739dced5b9163e094656ed3277c4898
digitalunites <pr.krasnianski@gmail.com>
2025-09-27 01:22:50 +0700

    lab3

---
+++ 24214/p.krasnyanskii/lab3/3.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <unistd.h>
+
+void getIds(){
+    printf("Your real id: %d\n", getuid());
+    printf("Your effective id: %d\n", geteuid());
+}
+
+void openFile(FILE *in){
+        in = fopen("file.txt", "r");
+        if (in == NULL)
+                perror("You haven't enough permissions to open this file\n");
+        else printf("File opened succesfully\n");
+        fclose(in);
+}
+
+int main(){
+         printf("\n");
+         FILE* in;
+
+         getIds();
+         openFile(in);
+
+         setuid(getuid());
+
+         getIds();
+         openFile(in);
+
+         return 0;
+}
