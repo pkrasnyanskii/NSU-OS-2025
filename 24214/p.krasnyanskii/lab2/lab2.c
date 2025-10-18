d9a68a2f7719525461b8ea9d5e295f0cc22275f4
digitalunites <pr.krasnianski@gmail.com>
2025-10-18 11:36:10 +0700

    Added empty line

--- 24214/p.krasnyanskii/lab2/lab2.c
+++ 24214/p.krasnyanskii/lab2/lab2.c
@@ -1,24 +1,26 @@
-#include <sys/types.h>
-#include <stdio.h>
-#include <time.h>
-#include <stdlib.h>
+    #include <sys/types.h>
+    #include <stdio.h>
+    #include <time.h>
+    #include <stdlib.h>
 
-extern char *tzname[];
+    extern char *tzname[];
 
-main() {
-    putenv("TZ=America/Los_Angeles");
-    time_t now;
-    struct tm *sp;
+    main() {
+        putenv("TZ=America/Los_Angeles");
+        time_t now;
+        struct tm *sp;
 
-    (void) time(&now);
+        (void) time(&now);
 
-    printf("%s", ctime(&now));
+        printf("%s", ctime(&now));
 
-    sp = localtime(&now);
-    printf("%d/%d/%02d %d:%02d %s\n",
-           sp->tm_mon + 1, sp->tm_mday,
-           sp->tm_year + 1900, sp->tm_hour,
-           sp->tm_min, tzname[sp->tm_isdst]);
+        sp = localtime(&now);
+        printf("%d/%d/%02d %d:%02d %s\n",
+            sp->tm_mon + 1, sp->tm_mday,
+            sp->tm_year + 1900, sp->tm_hour,
+            sp->tm_min, tzname[sp->tm_isdst]);
 
-    exit(0);
-}
+        exit(0);
+    }
+
+    
