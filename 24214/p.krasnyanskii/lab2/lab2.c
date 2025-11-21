ea624a52b1899a01c1680bfd71dacfbff3bb0d22
digitalunites <pr.krasnianski@gmail.com>
2025-11-21 14:45:03 +0700

    Proper main signature

--- 24214/p.krasnyanskii/lab2/lab2.c
+++ 24214/p.krasnyanskii/lab2/lab2.c
@@ -1,26 +1,23 @@
-    #include <sys/types.h>
-    #include <stdio.h>
-    #include <time.h>
-    #include <stdlib.h>
+#include <sys/types.h>
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+extern char *tzname[];
 
-    extern char *tzname[];
+int main(void) {
+    putenv("TZ=America/Los_Angeles");
+    time_t now;
+    struct tm *sp;
 
-    main() {
-        putenv("TZ=America/Los_Angeles");
-        time_t now;
-        struct tm *sp;
+    (void) time(&now);
+    printf("%s", ctime(&now));
 
-        (void) time(&now);
-
-        printf("%s", ctime(&now));
-
-        sp = localtime(&now);
-        printf("%d/%d/%02d %d:%02d %s\n",
-            sp->tm_mon + 1, sp->tm_mday,
-            sp->tm_year + 1900, sp->tm_hour,
-            sp->tm_min, tzname[sp->tm_isdst]);
-
-        exit(0);
-    }
+    sp = localtime(&now);
+    printf("%d/%d/%02d %d:%02d %s\n",
+        sp->tm_mon + 1, sp->tm_mday,
+        sp->tm_year + 1900, sp->tm_hour,
+        sp->tm_min, tzname[sp->tm_isdst]);
+    exit(0);
+}
 
     
